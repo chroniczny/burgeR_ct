4d44c23f5be0fb36141b278376d9dd5cbb1fbe24
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-08-03 22:17:49 +0200

    destroying interceptors to prevent creating them every component with errorhandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,14 +10,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
         // componentDidMount () { // after rendering child components
         componentWillMount () { // before rendering child components
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            // console.log('Will UNmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
