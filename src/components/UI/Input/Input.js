f8475fc44f279aeb4395c90480ff1a0755af96e1
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-08-22 21:23:00 +0200

    contact-data inputs with select input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,22 +5,40 @@ const input = (props) => {
     let inputElement = null;
     switch (props.inputtype) {
         case('input'):
+            console.log('input');
+
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
             break;
         case('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
+            break;
+        case('select'):
+            console.log('select');
+            console.log('select');
+            inputElement =
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option
+                            key={option.value}
+                            value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>;
             break;
         default:
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
     }
 
     return (
